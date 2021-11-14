3024a06731b18c4fd41092ddb84523b0c63b566f
udarasan <udarassanjeewa@gmail.com>
2021-11-14 13:36:15 +0530

    intial commit

---
+++ src/main/java/com/example/simpleroot/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.simpleroot.controller;
+
+import com.example.simpleroot.dto.UserDTO;
+import com.example.simpleroot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "Simple-Root";
+    }
+
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
+    }
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "user Updated!";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser() {
+        return "User Deleted!";
+    }
+}
